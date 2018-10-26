bf98cb054e6c338a4667a4168c3a5d974dd2f3d0
Bilal Eraslan <beraslan@muhasebat.gov.tr>
2018-10-26 10:17:28 +0300

    init

---
+++ src/main/java/com/be/microparent/MicroParentApplication.java
@@ -0,0 +1,12 @@
+package com.be.microparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroParentApplication.class, args);
+	}
+}
